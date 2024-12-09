4d7d1be2db14cba4d5ab9b6e3db7a6a463d74177
Slawomir Wieczorek <slawomir.wieczorek@bedag.ch>
2024-12-09 15:32:45 +0100

    HELLODATA-1881 - sftpgo sidecar init

---
+++ hello-data-sidecars/hello-data-sidecar-sftpgo/src/main/java/ch/bedag/dap/hellodata/sidecars/sftpgo/HDSidecarSftpgo.java
@@ -0,0 +1,40 @@
+package ch.bedag.dap.hellodata.sidecars.sftpgo;
+
+import ch.bedag.dap.hellodata.commons.nats.annotation.EnableJetStream;
+import ch.bedag.dap.hellodata.commons.sidecars.context.HelloDataContextConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJetStream
+@EnableScheduling
+@SpringBootApplication
+@EnableDiscoveryClient
+@ConfigurationPropertiesScan
+@ComponentScan("ch.bedag.dap.hellodata")
+@EnableConfigurationProperties({HelloDataContextConfig.class})
+public class HDSidecarSftpgo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HDSidecarSftpgo.class, args);
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder() {
+            @Override
+            public void configure(ObjectMapper objectMapper) {
+                super.configure(objectMapper);
+                objectMapper.registerModule(new JavaTimeModule());
+            }
+        };
+    }
+}
